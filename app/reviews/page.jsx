d4acbf1ecd7e3a4c9f5318064e70be4a6aabec2d
sa5040 <sr.sanjeev@hotmail.com>
2023-10-18 17:20:22 +0530

    link nested route

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const ReviewPage = () => {
   return (
     <>
       <h1>Review</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/reviews/hollow-knight">Hollow Knight</Link>
+          </li>
+          <li>
+            <Link href="/reviews/stardew-valley">Stardew Valley</Link>
+          </li>
+        </ul>
+      </nav>
       <p>Here we will list the reviews.</p>
     </>
   );
